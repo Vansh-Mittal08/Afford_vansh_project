741cae1362061ef55296e092b0119862513bd172
Vansh-Mittal08 <vanshmittal080904@gmail.com>
2025-04-12 15:24:21 +0530

    finalcommit

--- server/api/weather.js
+++ server/api/weather.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 const WEATHER = require("../models/Weather");
 
-// Configuring the path to read the environment variable file, .env, to get the weather api key
+
 require('dotenv').config({path: "./../../../.env"});
 
 const baseUrl = "http://api.openweathermap.org/data/2.5/weather";
@@ -18,16 +18,9 @@ class Weather {
      */
     getWeatherData = async (zipCode, tempMetric) => {
 
-        /**
-         * Use get api for "By ZIP code" (https://openweathermap.org/current#zip)
-         * - The "us" query stands for "United States
-         * - "process.env.WEATHER_KEY" is the api key that we get from the .env file
-         * - "units" query can be either imperial (Fahrenheit) or metric (Celsius)
-         */
         let url = `${baseUrl}?zip=${zipCode},us&appid=${process.env.WEATHER_KEY}&units=${tempMetric}`;
 
-        // Awaitable call to get the information from the weather api and then return the data.
-        // TODO: Add error handling for this call
+       
         return (await axios(url)).data;
     }
 
